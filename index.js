2085ca5b3eb1fc95c95ac645fec1beaf9572e401
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-18 01:32:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const p = document.querySelector("p")
+console.log(p.textContent)
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    p.textContent="This is really cool!"
+  });
